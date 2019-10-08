e0bffbae3fc010df6dddde4fa3b1c5aa4875d9c1
XDargu <xdargu@gmail.com>
2019-10-09 00:16:27 +0200

    Timeline basic controls and multiple bug fixes

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,6 +5,131 @@ import * as BASICO from './ui/ui';
 import * as NET_TYPES from './network/types';
 import SceneController from './render/sceneController';
 
+class PlaybackController {
+
+    private renderer: Renderer;
+    private isPlaying: boolean;
+    private elapsedTime: number;
+
+    constructor(renderer: Renderer) {
+        this.isPlaying = false;
+        this.elapsedTime = 0;
+        this.renderer = renderer;
+    }
+
+    update(elapsedSeconds: number) {
+        if (this.isPlaying)
+        {
+            this.elapsedTime += elapsedSeconds;
+
+            // Check how many frames we have to skip
+            let currentFrame = this.renderer.getCurrentFrame();
+            let currentElapsedTime = this.renderer.getElapsedTimeOfFrame(currentFrame);
+
+            while (this.elapsedTime > currentElapsedTime && currentFrame < this.renderer.getFrameCount() - 1)
+            {
+                currentFrame++;
+                this.elapsedTime -= currentElapsedTime;
+                currentElapsedTime = this.renderer.getElapsedTimeOfFrame(currentFrame);
+            }
+
+            this.renderer.applyFrame(currentFrame);
+
+            // Stop in the last frame
+            if (currentFrame == this.renderer.getFrameCount() - 1)
+            {
+                this.stopPlayback();
+            }
+        }
+    }
+
+    updateUI()
+    {
+        if (this.isPlaying) {
+            document.getElementById("timeline-play-icon").classList.remove("fa-play");
+            document.getElementById("timeline-play-icon").classList.add("fa-stop");
+        }
+        else {
+            document.getElementById("timeline-play-icon").classList.remove("fa-stop");
+            document.getElementById("timeline-play-icon").classList.add("fa-play");
+        }
+
+        if (this.renderer.getCurrentFrame() == 0) {
+            document.getElementById("timeline-first").classList.add("basico-disabled");
+            document.getElementById("timeline-prev").classList.add("basico-disabled");
+        }
+        else {
+            document.getElementById("timeline-first").classList.remove("basico-disabled");
+            document.getElementById("timeline-prev").classList.remove("basico-disabled");
+        }
+
+        if (this.renderer.getCurrentFrame() == this.renderer.getFrameCount() - 1) {
+            document.getElementById("timeline-last").classList.add("basico-disabled");
+            document.getElementById("timeline-next").classList.add("basico-disabled");
+        }
+        else {
+            document.getElementById("timeline-last").classList.remove("basico-disabled");
+            document.getElementById("timeline-next").classList.remove("basico-disabled");
+        }
+    }
+
+    startPlayback()
+    {
+        this.isPlaying = true;
+        this.updateUI();
+    }
+
+    stopPlayback()
+    {
+        this.isPlaying = false;
+        this.elapsedTime = 0;
+        this.updateUI();
+    }
+
+    onTimelineNextClicked()
+    {
+        if (this.renderer.getCurrentFrame() < this.renderer.getFrameCount() - 1) {
+            this.renderer.applyFrame(this.renderer.getCurrentFrame() + 1);
+            this.updateUI();
+        }
+    }
+
+    onTimelinePrevClicked()
+    {
+        if (this.renderer.getCurrentFrame() > 0) {
+            this.renderer.applyFrame(this.renderer.getCurrentFrame() - 1);
+            this.updateUI();
+        }
+    }
+
+    onTimelineFirstClicked()
+    {
+        if (this.renderer.getCurrentFrame() != 0) {
+            this.renderer.applyFrame(0);
+            this.updateUI();
+        }
+    }
+
+    onTimelineLastClicked()
+    {
+        if (this.renderer.getCurrentFrame() != this.renderer.getFrameCount() - 1) {
+            this.renderer.applyFrame(this.renderer.getFrameCount() - 1);
+            this.updateUI();
+        }
+    }
+
+    onTimelinePlayClicked()
+    {
+        if (!this.isPlaying)
+        {
+            this.startPlayback();
+        }
+        else
+        {
+            this.stopPlayback();
+        }
+    }
+}
 
 export default class Renderer {
     private sceneController: SceneController;
@@ -23,7 +148,12 @@ export default class Renderer {
     // Networking
     private connectionsList: ConnectionsList;
 
+    // Playback
+    private playbackController: PlaybackController;
+
     initialize(canvas: HTMLCanvasElement) {
+
+        this.playbackController = new PlaybackController(this);
 
         this.sceneController = new SceneController();
         this.sceneController.initialize(canvas);
@@ -37,7 +167,7 @@ export default class Renderer {
         this.recordedData = new RECORDING.NaiveRecordedData();
         this.recordedData.addTestData();
 
-        this.timeline.length = this.recordedData.getSize();
+        this.timeline.updateLength(this.recordedData.getSize());
 
         let connectionsListElement: HTMLElement = document.getElementById(`connectionsList`);
         this.connectionsList = new ConnectionsList(connectionsListElement, this.onMessageArrived.bind(this));
@@ -77,6 +207,13 @@ export default class Renderer {
                 document.getElementById("setting-list")
             ]
         );
+
+        // Create timeline callbacks
+        document.getElementById("timeline-play").onclick = this.playbackController.onTimelinePlayClicked.bind(this.playbackController);
+        document.getElementById("timeline-next").onclick = this.playbackController.onTimelineNextClicked.bind(this.playbackController);
+        document.getElementById("timeline-prev").onclick = this.playbackController.onTimelinePrevClicked.bind(this.playbackController);
+        document.getElementById("timeline-first").onclick = this.playbackController.onTimelineFirstClicked.bind(this.playbackController);
+        document.getElementById("timeline-last").onclick = this.playbackController.onTimelineLastClicked.bind(this.playbackController);
     }
 
     onMessageArrived(data: string) : void
@@ -113,7 +250,7 @@ export default class Renderer {
                     }
 
                     this.recordedData.pushFrame(frameToBuild);
-                    this.timeline.length = this.recordedData.getSize();
+                    this.timeline.updateLength(this.recordedData.getSize());
 
                     console.log(frameToBuild);
                 }
@@ -131,6 +268,7 @@ export default class Renderer {
         this.frameData = this.recordedData.buildFrameData(frame);
 
         this.timeline.currentFrame = frame;
+        this.playbackController.updateUI();
 
         console.log(this.frameData);
         console.log(this.recordedData);
@@ -288,11 +426,40 @@ export default class Renderer {
         let timelineWrapper: HTMLElement = document.getElementById('timeline-wrapper');
         this.timeline = new Timeline(timelineElement, timelineWrapper);
         this.timeline.setFrameClickedCallback(this.onTimelineClicked.bind(this));
+        this.timeline.setTimelineUpdatedCallback(this.onTimelineUpdated.bind(this));
     }
 
+    getCurrentFrame()
+    {
+        return this.timeline.currentFrame;
+    }
+
+    getFrameCount()
+    {
+        return this.timeline.length;
+    }
+
+    getElapsedTimeOfFrame(frame: number)
+    {
+        if (frame == this.getCurrentFrame())
+        {
+            return this.frameData.elapsedTime;
+        }
+        else
+        {
+            return this.recordedData.buildFrameData(frame).elapsedTime;
+        }
+    }
+
+    // Callbacks
     onTimelineClicked(frame: number)
     {
         this.applyFrame(frame);
+    }
+
+    onTimelineUpdated(elapsedSeconds: number)
+    {
+        this.playbackController.update(elapsedSeconds);
     }
 }
 
