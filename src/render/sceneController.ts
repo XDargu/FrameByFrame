6e6e06980cc8127653ba93c1e2f17b982ff0971d
XDargu <xdargu@gmail.com>
2021-04-06 22:47:04 +0200

    Basic gizmo to display position

--- src/render/sceneController.ts
+++ src/render/sceneController.ts
@@ -1,8 +1,6 @@
 import * as BABYLON from 'babylonjs';
 import * as RECORDING from '../recording/RecordingData';
 import { GridMaterial } from 'babylonjs-materials';
-import { Quaternion } from 'babylonjs';
-import { Mesh } from 'babylonjs/Meshes/index';
 
 export interface IEntitySelectedCallback
 {
@@ -567,6 +565,116 @@ class LayerManager
     }
 }
 
+class ArrowGizmo extends BABYLON.Gizmo
+{
+    private _coloredMaterial: BABYLON.StandardMaterial;
+    private _gizmoMesh: BABYLON.Mesh;
+    
+    constructor(gizmoLayer: BABYLON.UtilityLayerRenderer = BABYLON.UtilityLayerRenderer.DefaultUtilityLayer, axis: BABYLON.Vector3, color: BABYLON.Color3 = BABYLON.Color3.Gray(), thickness: number = 1) {
+        super(gizmoLayer);
+
+        // Create Material
+        this._coloredMaterial = new BABYLON.StandardMaterial("", gizmoLayer.utilityLayerScene);
+        this._coloredMaterial.diffuseColor = color;
+        this._coloredMaterial.specularColor = color.subtract(new BABYLON.Color3(0.1, 0.1, 0.1));
+
+        // Build Mesh + Collider
+        const arrow = ArrowGizmo._CreateArrow(gizmoLayer.utilityLayerScene, this._coloredMaterial, thickness);
+        const collider = ArrowGizmo._CreateArrow(gizmoLayer.utilityLayerScene, this._coloredMaterial, thickness + 4, true);
+
+        // Add to Root Node
+        this._gizmoMesh = new BABYLON.Mesh("", gizmoLayer.utilityLayerScene);
+        this._gizmoMesh.addChild((arrow as BABYLON.Mesh));
+        this._gizmoMesh.addChild((collider as BABYLON.Mesh));
+
+        this._gizmoMesh.lookAt(this._rootMesh.position.add(axis));
+        this._gizmoMesh.scaling.scaleInPlace(1 / 3);
+        this._gizmoMesh.parent = this._rootMesh;
+
+        var light = gizmoLayer._getSharedGizmoLight();
+        light.includedOnlyMeshes = light.includedOnlyMeshes.concat(this._rootMesh.getChildMeshes(false));
+    }
+
+    public static _CreateArrow(scene: BABYLON.Scene, material: BABYLON.StandardMaterial, thickness: number = 1, isCollider = false): BABYLON.TransformNode {
+        const arrow = new BABYLON.TransformNode("arrow", scene);
+        let cylinder = BABYLON.CylinderBuilder.CreateCylinder("cylinder", { diameterTop: 0, height: 0.075, diameterBottom: 0.0375 * (1 + (thickness - 1) / 4), tessellation: 96 }, scene);
+        let line = BABYLON.CylinderBuilder.CreateCylinder("cylinder", { diameterTop: 0.005 * thickness, height: 0.275, diameterBottom: 0.005 * thickness, tessellation: 96 }, scene);
+
+        // Position arrow pointing in its drag axis
+        cylinder.parent = arrow;
+        cylinder.material = material;
+        cylinder.rotation.x = Math.PI / 2;
+        cylinder.position.z += 0.3;
+
+        line.parent = arrow;
+        line.material = material;
+        line.position.z += 0.275 / 2;
+        line.rotation.x = Math.PI / 2;
+
+        if (isCollider) {
+            line.visibility = 0;
+            cylinder.visibility = 0;
+        }
+        return arrow;
+    }
+
+    public setAxis(axis: BABYLON.Vector3) {
+        this._gizmoMesh.lookAt(axis);
+    }
+
+    public dispose() {
+        if (this._gizmoMesh) {
+            this._gizmoMesh.dispose();
+        }
+        this._coloredMaterial.dispose();
+        super.dispose();
+    }
+}
+
+class AxisGizmo extends BABYLON.Gizmo
+{
+    private xGizmo: ArrowGizmo;
+    private yGizmo: ArrowGizmo;
+    private zGizmo: ArrowGizmo;
+    private beforeRenderObserver: BABYLON.Observer<BABYLON.Scene>;
+    
+    constructor(gizmoLayer: BABYLON.UtilityLayerRenderer = BABYLON.UtilityLayerRenderer.DefaultUtilityLayer, thickness: number = 1) {
+        super(gizmoLayer);
+
+        this.xGizmo = new ArrowGizmo(gizmoLayer, new BABYLON.Vector3(1, 0, 0), BABYLON.Color3.Red().scale(0.5), thickness);
+        this.yGizmo = new ArrowGizmo(gizmoLayer, new BABYLON.Vector3(0, 1, 0), BABYLON.Color3.Green().scale(0.5), thickness);
+        this.zGizmo = new ArrowGizmo(gizmoLayer, new BABYLON.Vector3(0, 0, 1), BABYLON.Color3.Blue().scale(0.5), thickness);
+
+        this.beforeRenderObserver = this.gizmoLayer.utilityLayerScene.onBeforeRenderObservable.add(() => {
+            /*if (this.xGizmo.attachedMesh) {
+                this.xGizmo.setAxis(this.xGizmo.attachedMesh.forward);
+            }
+            if (this.yGizmo.attachedMesh) {
+                this.yGizmo.setAxis(this.yGizmo.attachedMesh.up);
+            }
+            if (this.zGizmo.attachedMesh) {
+                this.zGizmo.setAxis(this.zGizmo.attachedMesh.right);
+            }*/
+        });
+    }
+
+    public attachToMesh(mesh: BABYLON.Mesh) {
+        [this.xGizmo, this.yGizmo, this.zGizmo].forEach((gizmo) => {
+            gizmo.attachedMesh = mesh;
+        });
+    }
+
+    public dispose() {
+        [this.xGizmo, this.yGizmo, this.zGizmo].forEach((gizmo) => {
+            gizmo.dispose();
+        });
+        if (this.beforeRenderObserver) {
+            this.gizmoLayer.utilityLayerScene.onBeforeRenderObservable.remove(this.beforeRenderObserver);
+        }
+        super.dispose();
+    }
+}
+
 export default class SceneController
 {
     private _canvas: HTMLCanvasElement;
@@ -601,6 +709,9 @@ export default class SceneController
     private spherePool: SpherePool;
     private boxPool: BoxPool;
     private planePool: PlanePool;
+
+    // Gizmos
+    private axisGizmo: AxisGizmo;
 
     removeAllProperties()
     {
@@ -695,7 +806,7 @@ export default class SceneController
             rotationMatrix.setRow(0, new BABYLON.Vector4(right.x, right.y, right.z, 0));
             rotationMatrix.setRow(1, new BABYLON.Vector4(up.x, up.y, up.z, 0));
             rotationMatrix.setRow(2, new BABYLON.Vector4(forward.x, forward.y, forward.z, 0));
-            capsule.rotationQuaternion = new Quaternion();
+            capsule.rotationQuaternion = new BABYLON.Quaternion();
             capsule.rotationQuaternion.fromRotationMatrix(rotationMatrix);
 
             capsule.material = this.materialPool.getMaterialByColor(capsuleProperty.color);
@@ -735,7 +846,7 @@ export default class SceneController
             rotationMatrix.setRow(0, new BABYLON.Vector4(right.x, right.y, right.z, 0));
             rotationMatrix.setRow(1, new BABYLON.Vector4(up.x, up.y, up.z, 0));
             rotationMatrix.setRow(2, new BABYLON.Vector4(forward.x, forward.y, forward.z, 0));
-            oobb.rotationQuaternion = new Quaternion();
+            oobb.rotationQuaternion = new BABYLON.Quaternion();
             oobb.rotationQuaternion.fromRotationMatrix(rotationMatrix);
 
             oobb.material = this.materialPool.getMaterialByColor(oobbProperty.color);
@@ -761,7 +872,7 @@ export default class SceneController
             rotationMatrix.setRow(0, new BABYLON.Vector4(right.x, right.y, right.z, 0));
             rotationMatrix.setRow(1, new BABYLON.Vector4(up.x, up.y, up.z, 0));
             rotationMatrix.setRow(2, new BABYLON.Vector4(forward.x, forward.y, forward.z, 0));
-            plane.rotationQuaternion = new Quaternion();
+            plane.rotationQuaternion = new BABYLON.Quaternion();
             plane.rotationQuaternion.fromRotationMatrix(rotationMatrix);
 
             plane.material = this.materialPool.getMaterialByColor(planeProperty.color);
@@ -836,6 +947,8 @@ export default class SceneController
             mesh.outlineWidth = 0.03;
             mesh.renderOutline = true;
         });
+
+        this.axisGizmo.attachToMesh(entity.mesh);
     }
 
     private applyHoverMaterial(entity: IEntityData)
@@ -976,7 +1089,11 @@ export default class SceneController
         this.boxPool = new BoxPool(this._scene);
         this.planePool = new PlanePool(this._scene);
         this.layerManager = new LayerManager();
-        
+
+        // Gizmos
+        // Create utility layer the gizmo will be rendered on
+        let utilLayer = new BABYLON.UtilityLayerRenderer(scene);
+        this.axisGizmo = new AxisGizmo(utilLayer);
 
         // This creates and positions a free camera (non-mesh)
         //const camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 8, 50, BABYLON.Vector3.Zero(), scene);
